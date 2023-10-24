cda0dde936ec75fc4f15c27cc9651c398ea23f06
22A91A16H8 <119839906+22A91A16H8@users.noreply.github.com>
2023-10-24 16:07:02 +0530

    import java.util.Scanner;
    public class Main {
       public static void main(String[] args) {
           Scanner scanner = new Scanner(System.in);
           int n = scanner.nextInt();
           scanner.close();
    
           if (isPrime(n)) {
               System.out.println("Prime");
           } else {
               System.out.println("Not Prime");
           }
       }
    
       public static boolean isPrime(int n) {
           if (n <= 1) {
               return false;
           }
           for (int i = 2; i < Math.sqrt(n) + 1; i++) {
               if (n % i == 0) {
                   return false;
               }
           }
           return true;
       }
    }

---
+++ Prime.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       int n = scanner.nextInt();
+       scanner.close();
+
+       if (isPrime(n)) {
+           System.out.println("Prime");
+       } else {
+           System.out.println("Not Prime");
+       }
+   }
+
+   public static boolean isPrime(int n) {
+       if (n <= 1) {
+           return false;
+       }
+       for (int i = 2; i < Math.sqrt(n) + 1; i++) {
+           if (n % i == 0) {
+               return false;
+           }
+       }
+       return true;
+   }
+}
