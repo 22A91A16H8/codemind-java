8d4868ee6ca4a05e49b81305157e198ec2b9a0ca
22A91A16H8 <119839906+22A91A16H8@users.noreply.github.com>
2023-11-06 19:13:14 +0530

    import java.util.*; public class Main {     static int reverseDigits(int num)     {         int rev = 0;         while (num > 0)         {             rev = rev * 10 + num % 10;             num /= 10;         }         return rev;     }     static int square(int num)     {         return (num * num);     }     static boolean checkAdamNumber(int num)     {         int a = square(num);         int b = square(reverseDigits(num));         if (a == reverseDigits(b))         return true;         return false;     }     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int num = sc.nextInt();         if (checkAdamNumber(num))         System.out.println("True");         else         System.out.println("False");     }
    }

---
+++ Adam_number.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main { 
+    static int reverseDigits(int num) 
+    { 
+        int rev = 0; 
+        while (num > 0) 
+        { 
+            rev = rev * 10 + num % 10; 
+            num /= 10; 
+        } 
+        return rev; 
+    } 
+    static int square(int num) 
+    { 
+        return (num * num); 
+    } 
+    static boolean checkAdamNumber(int num) 
+    { 
+        int a = square(num); 
+        int b = square(reverseDigits(num)); 
+        if (a == reverseDigits(b)) 
+        return true; 
+        return false;         
+    } 
+    public static void main(String[] args) 
+    { 
+        Scanner sc=new Scanner(System.in);
+        int num = sc.nextInt(); 
+        if (checkAdamNumber(num)) 
+        System.out.println("True"); 
+        else
+        System.out.println("False");     
+    } 
+  
+} 
