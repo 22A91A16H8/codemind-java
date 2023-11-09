47ef8f625b06903a848c730ff8b9a7566372f199
22A91A16H8 <119839906+22A91A16H8@users.noreply.github.com>
2023-11-09 19:57:21 +0530

    import java.util.Scanner; public class Test {     static boolean check(int n)     {         int count_digits = Integer.toString(n).length();         int sum = 0;         int x = n;         while (x!=0)         {             int r = x%10;             sum = (int) (sum + Math.pow(r, count_digits--));             x = x/10;         }         return (sum == n);     }     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n = sc.nextInt();         System.out.println(check(n) ? "True" : "False");     } }

---
+++ Disarium_number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Test
+{
+    static boolean check(int n)
+    {
+        int count_digits = Integer.toString(n).length();
+        int sum = 0; 
+        int x = n;
+        while (x!=0)
+        {
+            int r = x%10;
+            sum = (int) (sum + Math.pow(r, count_digits--));
+            x = x/10;
+        }
+        return (sum == n);
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(check(n) ? "True" : "False");
+    }
+}
